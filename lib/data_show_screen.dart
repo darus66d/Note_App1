4164836b795fb481de7213cef4d32d4ced92dc33
Darus Salam <darussalam66d@gmail.com>
2025-04-21 07:19:34 +0600

    update

---
+++ lib/data_show_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DataShowScreen extends StatelessWidget {
+  const DataShowScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Data Show Screen"),
+      ),
+      body: Column(
+        children: [
+
+
+
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:note_app1/data_show_screen.dart';
 import 'firebase_options.dart';
 
 
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Note_App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -33,15 +35,43 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: Center(child: ElevatedButton(onPressed: (){
-        firestore.collection("users").add({
-          "userName":"DarusSalam",
-          "age":"22",
-          "class" : "1",
-        }).then((value)=> print("Data Added"))
-            .catchError((error) => print("Failed to add user: $error"));
-      },
-          child: Text("Add data")
+      backgroundColor: Colors.blueGrey,
+      body: Center(child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ElevatedButton(onPressed: (){
+            firestore.collection("users").doc("01").set({
+              "userName":"DarusEmon",
+              "age":"27",
+              "class" : "16",
+            }).then((value)=> print("Data Added"))
+                .catchError((error) => print("Failed to add user: $error"));
+          }, child: const Text("Set data"),),
+
+
+
+          ElevatedButton(onPressed: (){
+            firestore.collection("restoreusers").doc("02").set({
+              "userName":"DarusEmon",
+              "age":"27",
+              "class" : "16",
+            }).then((value)=>print("Data Restore"))
+                .catchError((error)=>print("Failed to restore data : $error"));
+          }, child: Text("Data restore")),
+
+          ElevatedButton(onPressed: (){
+            firestore.collection("extra").add({
+              "userName":"Darul karar",
+              "age":"18",
+              "class" : "10",
+            }).then((value)=> print("Data Added"))
+                .catchError((error) => print("Failed to add user: $error"));
+          }, child: Text("Add data"),),
+          
+          ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>DataShowScreen()));
+          }, child: Text("Next_Screen"))
+        ],
       )
       ),
     );
