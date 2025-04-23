d8c9111ec4c3675deb7ac445a219a7f1b275d3a1
Darus Salam <darussalam66d@gmail.com>
2025-04-23 18:21:30 +0600

    update

--- lib/data_show_screen.dart
+++ lib/data_show_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:share_plus/share_plus.dart';
 
 class DataShowScreen extends StatelessWidget {
   const DataShowScreen({super.key});
@@ -7,14 +10,37 @@ class DataShowScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Data Show Screen"),
+        title: const Text("Data Show Screen"),
       ),
-      body: Column(
-        children: [
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection("users").snapshots(),
+          builder: (BuildContext context,AsyncSnapshot<QuerySnapshot>sanpshots){
 
+            if(sanpshots.hasError){
+              return const Center(child: CircularProgressIndicator());
+            }
+            if(sanpshots.connectionState==ConnectionState.waiting){
+              return const Center(child: CircularProgressIndicator());
+            }
+            return ListView.builder(
+              itemCount: sanpshots.data!.docs.length,
+                itemBuilder: (context,index){
 
+                var doc = sanpshots.data!.docs[index];
 
-        ],
+                return ListTile(
+                  leading: IconButton(onPressed: (){
+                    Clipboard.setData(ClipboardData(text: doc["userName"]));
+                  }, icon:const Icon(Icons.copy) ),
+                  title: Text("Name:${doc["userName"]}"),
+                  subtitle: Text("age:${doc["age"]}"),
+                  trailing: IconButton(onPressed: (){
+                    Share.share('check out my website https://example.com');
+                  }, icon: Icon(Icons.share)),
+                );
+                }
+            );
+          },
       ),
     );
   }
